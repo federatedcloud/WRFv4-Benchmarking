e8a6ae9f584557a254124cf6fbfc00d0d409c488
sk8forether <pzv2@cornell.edu>
2021-06-10 06:43:32 +0000

    Lots of environment variable changes, parallel HDF5 almost configures (fails at MPI link test)

--- Docker/docker-build.sh
+++ Docker/docker-build.sh
@@ -3,7 +3,7 @@
 
 export BUILD_OUTPUT_FILE=./build_output.txt
 echo "Build started, output in ${BUILD_OUTPUT_FILE}"
-docker build --network=host ./ > $BUILD_OUTPUT_FILE
+docker build --network=host --tag cornellcac/wrf:4.2.2 ./ > $BUILD_OUTPUT_FILE
 echo "Build complete.  Check output file for final status.  Errors:"
 echo "-------------------------------------------------------------"
 cat build_output.txt | grep ERROR
