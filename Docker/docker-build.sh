a96a6c89db01917d546442e49a94c0249443565f
sk8forether <pzv2@cornell.edu>
2021-06-23 03:00:49 +0000

    SUCCESSFUL COMPILE OF WRF in Docker

--- Docker/docker-build.sh
+++ Docker/docker-build.sh
@@ -5,10 +5,10 @@ export BUILD_OUTPUT_FILE=./build_output.txt
 echo "Build started, output in ${BUILD_OUTPUT_FILE}"
 
 # Save build output log instead of printing to the screen
-docker build --network=host --tag cornellcac/wrf:4.2.2 ./ 2>&1 > $BUILD_OUTPUT_FILE
+#docker build --network=host --tag cornellcac/wrf:4.2.2 ./ 2>&1 > $BUILD_OUTPUT_FILE
 
 # Uncomment below to save build output log AND print to screen
-#docker build --network=host --tag cornellcac/wrf:4.2.2 ./ 2>&1 | tee $BUILD_OUTPUT_FILE
+docker build --network=host --tag cornellcac/wrf:4.2.2 ./ 2>&1 | tee $BUILD_OUTPUT_FILE
 
 # Some useful summary output
 echo "-------------------------------------------------------------"
