f372eeb2f2e0afa5c88a36f9f3086547c24a6215
sk8forether <pzv2@cornell.edu>
2021-07-28 19:47:40 +0000

    Renamed Intel versions to differentiate between upcoming GNU version

--- Docker/intel-build.sh
+++ Docker/intel-build.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-# A simple script for building the container
+# A simple script for building the Intel (with Intel MPI) container
 
-export BUILD_OUTPUT_FILE=./build_output.txt
+export BUILD_OUTPUT_FILE=./output_intel_build.txt
 echo "Build started, output in ${BUILD_OUTPUT_FILE}"
 
 # Save build output log instead of printing to the screen
-#docker build --network=host --tag cornellcac/wrf:4.2.2 ./ 2>&1 > $BUILD_OUTPUT_FILE
+#docker build --network=host --tag cornellcac/wrf:4.2.2-intel-7415915e0b8e - < Dockerfile-Intel 2>&1 > $BUILD_OUTPUT_FILE
 
 # Uncomment below to save build output log AND print to screen
-docker build --network=host --tag cornellcac/wrf:4.2.2-intel-7415915e0b8e ./ 2>&1 | tee $BUILD_OUTPUT_FILE
+docker build --network=host --tag cornellcac/wrf:4.2.2-intel-7415915e0b8e - < Dockerfile-Intel 2>&1 | tee $BUILD_OUTPUT_FILE
 
 # Some useful summary output
 echo "-------------------------------------------------------------"
